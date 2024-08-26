fe3a4152f8100de29746915fd62530b6021a43ca
jieun <jangji0513@naver.com>
2024-08-26 09:57:42 +0900

    [FEAT] 로그인 페이지 전체 화면 레이아웃 구현 및 사이드바, 헤더 동적 렌더링 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
-import './App.css'
+import Login from './components/Login';
+import './App.css';
 
-const App = () => {
+const AppContent = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login'; // 현재 경로가 /login 인지 확인
+
   return (
-    <div style={{ display: 'flex' }}>
-      <Sidebar />
-      <div>
-        <Header />
-        <div style={{ padding: '20px', flexGrow: 1 }}>
-          <div style={{ display: 'flex' }}>
-            
-          </div>
+    <div>
+      {!isLoginPage && <Sidebar />}
+      {/* 로그인 페이지가 아닌 경우에만 사이드바를 렌더링 */}
+      <div style={{ flexGrow: 1 }}>
+        {!isLoginPage && <Header />}
+        {/* 로그인 페이지가 아닌 경우에만 헤더를 렌더링 */}
+        <div style={{ padding: isLoginPage ? '0' : '20px' }}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            {/* 다른 라우트를 추가하여 여러 페이지를 렌더링 할 수 있음 */}
+          </Routes>
         </div>
       </div>
     </div>
   );
 };
+
+const App = () => (
+  <Router>
+    {/* Router로 AppContent를 감싸서 useLocation 훅이 정상적으로 동작하도록 설정 */}
+    <AppContent />
+  </Router>
+);
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/Login.css';
+
+function Login() {
+    return (
+        <div className="login-container">
+            <div className="login-box">
+                <h1 className="logo">MainYourTrip</h1>
+                <form>
+                    <div className="input-group">
+                        <label htmlFor="username">아이디</label>
+                        <input type="text" id="username" placeholder="아이디를 입력해주세요" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">비밀번호</label>
+                        <input type="password" id="password" placeholder="비밀번호를 입력해주세요" />
+                    </div>
+                    <button type="submit" className="login-button">로그인</button>
+                </form>
+                <div className="signup-link">
+                    <a href="/signup">회원가입</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
