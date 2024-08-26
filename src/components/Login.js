68c2ddbb1c0192b5eaf9930dae984af785a6cf03
jieun <jangji0513@naver.com>
2024-08-26 13:07:28 +0900

    [ADD] : useNavigate훅 사용하여 main화면으로 리디렉션

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Login.css';
 
 const Login = () => {
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  const handleLogin = () => {
+    // 로그인 로직
+    // \로그인 인증 API 호출\
+    // 인증이 성공하면 아래 navigate를 호출
+
+    navigate('/');
+  };
+
   return (
     <div id="login-root" className="login-page">
       <h1 className="logo">MainYourTrip</h1>
@@ -14,7 +25,7 @@ const Login = () => {
           <label htmlFor="password">비밀번호</label>
           <input type="password" id="password" placeholder="비밀번호를 입력해주세요" />
         </div>
-        <button type="submit" className="login-button">로그인</button>
+        <button type="submit" className="login-button" onClick={handleLogin}>로그인</button>
         <div className="signup-link">
           <a href="/signup">회원가입</a>
         </div>
