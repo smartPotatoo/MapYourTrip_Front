f224098fb2db44d95519548a900bd647636c101d
jieun <jangji0513@naver.com>
2024-08-28 14:42:48 +0900

    [ADD] : 로그인 컴포넌트 백엔드 API 연동

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const [shake, setShake] = useState(false); // 애니메이션을 위한 상태
   const API_URL = process.env.REACT_APP_API_URL;
   console.log(API_URL);
-  const {handleSetToken} = useContext(MapYourTripContext);
+  const { handleSetToken } = useContext(MapYourTripContext);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -34,10 +34,12 @@ const Login = () => {
       if (response.ok) {
         const result = await response.json();
         handleSetToken(result.body.token);
-        // navigate('/'); // 메인 페이지로 이동
+        console.log("로그인 성공:", result);
+        navigate('/'); // 메인 페이지로 이동
       } else {
         const errorData = await response.json();
         setErrorMessage(errorData.message || '로그인에 실패했습니다.');
+        console.log("로그인 실패:", errorData);
         setShake(true); // 로그인 실패 시 애니메이션 시작
       }
     } catch (error) {
@@ -45,15 +47,6 @@ const Login = () => {
       setErrorMessage('서버와의 연결에 문제가 발생했습니다.');
       setShake(true); // 로그인 실패 시 애니메이션 시작
     }
-    
-
-    // 서버 연결 없이, 로그인 성공으로 간주하고 메인 페이지로 이동
-    // if (username && password) {
-    //   navigate('/');
-    // } else {
-    //   setErrorMessage('정확한 아이디와 비밀번호를 입력해주세요.');
-    //   setShake(true); // 로그인 실패 시 애니메이션 시작
-    // }
   };
 
   return (
