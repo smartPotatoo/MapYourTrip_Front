fe5ff54cb7f9b5c7f4d5b0c147376dc3db55ec38
민석 <47300870+seok019283501@users.noreply.github.com>
2024-08-28 13:18:20 +0900

    feat: add map marker

--- src/components/AddDetailSchedule.js
+++ src/components/AddDetailSchedule.js
@@ -4,6 +4,7 @@ import { IoIosSearch } from "react-icons/io";
 import axios from 'axios';
 import { useEffect, useState, useContext } from 'react';
 import MapYourTripContext from '../provider/MapYourTripContext';
+import { useDaumPostcodePopup } from "react-daum-postcode";
 const AddDetailSchedule = (props) => {
   const [addressInfo, setAddressInfo] = useState({
     startTime:'',
@@ -15,19 +16,25 @@ const AddDetailSchedule = (props) => {
   });
 
   const {handleSetScheduleTimeInfo} = useContext(MapYourTripContext);
-
   const [address,setAddress] = useState('');
+  const open = useDaumPostcodePopup("https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js");
   
-  const search = () =>{
-    axios.get((`/map-geocode/v2/geocode?query=${address}`),{
+  const selectAddress = (e) => {
+    open({ onComplete: handleComplete });
+  }
+
+  const handleComplete = (data) =>{
+    console.log(data)
+    axios.get((`/map-geocode/v2/geocode?query=${data.address}`),{
       headers:{
         "X-NCP-APIGW-API-KEY-ID":process.env.REACT_APP_NAVER_MAP_CLIENT_ID,
         "X-NCP-APIGW-API-KEY":process.env.REACT_APP_NAVER_MAP_API_KEY
       }
     }).then(res=>{
+      console.log(res)
       setAddressInfo({ ...addressInfo,
-        name: res.data.addresses[0].jibunAddress, 
-        address: res.data.addresses[0].roadAddress,
+        name: data.buildingName !== '' ? data.buildingName : data.address, 
+        address: data.address,
         x: res.data.addresses[0].x,
         y: res.data.addresses[0].y
       })
@@ -53,15 +60,15 @@ const AddDetailSchedule = (props) => {
   const handleSetEndTime=(e)=>{
     setAddressInfo({...addressInfo,endTime : e.target.value})
   }
-
+console.log(addressInfo)
   return(
     <>
       <div className="add-detail-schedule-container">
         <div className="add-detail-schedule-content-container">
           <div className="content-container">
             <label htmlFor='palce-name' className="content-name">여행 장소</label>
-            <input type="text" name='palce-name' className='palce-name' onChange={handleSetAddress}/>
-            <IoIosSearch size={25} className="searchIcon" onClick={()=>search()}/>
+            <input type="text" name='palce-name' className='palce-name' readOnly value={addressInfo.name}/>
+            <IoIosSearch size={25} className="searchIcon" onClick={()=>selectAddress()}/>
           </div>
           <div className="content-container">
             <label htmlFor="start-time" className="content-name">시작 시간</label>
--- src/components/DetailScheduleList.js
+++ src/components/DetailScheduleList.js
@@ -23,10 +23,8 @@ const DetailScheduleList = () => {
   useEffect(()=>{
     let startDate = detailScheduleInfo.startDate;
     let endDate = detailScheduleInfo.endDate
-    console.log(detailScheduleInfo)
 
     if(detailScheduleInfo.length !== 0 && type === 'create'){
-      console.log("adf")
       //시작날짜와 종료날짜에 따른 일일 날짜 리스트 생성
       const start = new Date(`20${startDate.slice(0, 2)}-${startDate.slice(2, 4)}-${startDate.slice(4, 6)}`);
       const end = new Date(`20${endDate.slice(0, 2)}-${endDate.slice(2, 4)}-${endDate.slice(4, 6)}`);
@@ -45,16 +43,23 @@ const DetailScheduleList = () => {
           currentDate.setDate(currentDate.getDate() + 1);
       }
       handleSetDateList(dates);
-      console.log(dates)
     }
     
   },[detailScheduleInfo])
 
   useEffect(()=>{
-    if(scheduleTimeInfo.lenth !== 0){
+    console.log(dateList)
+    if(scheduleTimeInfo.length !== 0){
       dateList.forEach((item)=>{
         if(item.date === date){
           item.times.push(scheduleTimeInfo)
+
+          item.times.sort((a, b) => {
+            const timeA = a.startTime.split(':').map(Number);
+            const timeB = b.startTime.split(':').map(Number);
+  
+            return timeA[0] - timeB[0] || timeA[1] - timeB[1];
+          });
         }
       })
       handleSetDateList([...dateList])
