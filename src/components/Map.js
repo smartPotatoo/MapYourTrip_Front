9247cd609616018ccea4f65dd83259862b7f6ec7
민석 <47300870+seok019283501@users.noreply.github.com>
2024-08-28 23:22:36 +0900

    feat: add marker info view

--- src/components/Map.js
+++ src/components/Map.js
@@ -26,7 +26,7 @@ useEffect(()=>{
     if(dateList.length !==0){
       dateList.forEach((date,dIdx)=>{
         date.times.forEach((time)=>{
-          time.dIdx = dIdx;
+          time.date = date.date;
           list.push(time);
         })
       })
@@ -37,12 +37,39 @@ useEffect(()=>{
 
   useEffect(()=>{
     if(timeList.length !== 0){
-
+      timeList.forEach((item)=>{
+        console.log(item)
+        let marker = new navermaps.Marker({
+          map: map,
+          position: new navermaps.LatLng(item.y, item.x)
+        });
+        const year = item.date.substring(0, 4);
+        const month = item.date.substring(4, 6);
+        const day = item.date.substring(6, 8);
+        let info = new navermaps.InfoWindow({
+          content: [
+              `<div class="map-marker-info" style='padding : 10px;'>`,
+              `   <div style='border-bottom : 1px solid black; margin-bottom: 3px; font-weight: 600; font-size: 1.2rem;'>${item.name}</div>`,
+              `   <div>${item.address}</div>`,
+              `   <div>${year}년 ${month}월 ${day}일</div>`,
+              `   <div>${item.startTime} ~ ${item.endTime}</div>`,
+              `   <div></div>`,
+              `</div>`
+          ].join('')
+        })
+        navermaps.Event.addListener(marker, "click", function(e) {
+          if (info.getMap()) {
+              info.close();
+          } else {
+              info.open(map, marker);
+          }
+        });
+      })
       map.updateBy(new navermaps.LatLng(timeList[0].y, timeList[0].x))
-    
 
     }
   },[timeList, map, navermaps])
+
 
   return (
     <NaverMap
@@ -50,15 +77,6 @@ useEffect(()=>{
       defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
       defaultZoom={15}
     >
-      {
-        timeList.map((item,index)=>(
-          <Marker key={index}
-            position={new navermaps.LatLng(item.y, item.x)}
-          />
-          
-        ))
-      }
-
     </NaverMap>
   );
 };
